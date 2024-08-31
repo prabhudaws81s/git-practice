594f1e74218e3e846c29a81d51918a21dc522f7e
Prabhu <prabhu4learning@gmail.com>
2024-08-31 23:16:55 +0530

    git commit with Arguments

---
+++ 05.VariablesPassword.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+read -s $person1 $person2
+
+echo "$person1 :: Hai $person2, How are you doing ? "
+echo "$person2 :: M Good $person1, Thank you!"
+echo "$person2 ::: How are you ?"
