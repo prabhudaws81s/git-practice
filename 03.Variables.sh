1e7a5511a04a083fd6d2c3faa1a297ff8382ab4a
Prabhu <prabhu4learning@gmail.com>
2024-08-31 23:06:26 +0530

    variables

---
+++ 03.Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=Ramesh
+person2=Suresh
+
+echo "$person1 :: Hai $person2, How are you doing ? "
+echo "$person2 :: M Good $person1, Thank you!"
+echo "$person2 ::: How are you ?"
