cb72cee321c621b2f4d3e8a40727626fbfb9b7e2
Prabhu <prabhu4learning@gmail.com>
2024-08-31 23:27:40 +0530

    git commit with Fruits

---
+++ 06.Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fruits=("Apple" "Banana" "cashew")
+
+echo " First fruit is :: ${fruits[0]} "
+echo " Second fruit is :: ${fruits[1]} "
+echo " Third fruit is :: ${fruits[2]} "
+echo " All fruit is :: ${fruits[@]} "
